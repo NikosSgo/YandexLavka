47decbde3429fe6b4fe9b3b8c743027706eb578f
SDaKko <danilkosenko172004@mail.ru>
2025-10-22 00:06:18 +0300

    Создана структура папок внутри проектов.

--- WareHouseService/WareHouse.Infrastructure/obj/Debug/net8.0/WareHouse.Infrastructure.AssemblyInfo.cs
+++ WareHouseService/WareHouse.Infrastructure/obj/Debug/net8.0/WareHouse.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WareHouse.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6375f203ce1327884ac372412efb2229a3bf7848")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2db2eb0e432176edb7f68c1571b1854fa6c642f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WareHouse.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WareHouse.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
