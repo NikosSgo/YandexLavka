fad010e13e336c4e8d9dd75f3f0c1261091ec0e0
NikosSgo <nikosgonnik@gmail.com>
2025-11-20 13:05:24 +0300

    Update entity in authservice domain and update gitignore

--- AuthService/AuthService.Domain/Entities/Entity.cs
+++ AuthService/AuthService.Domain/Entities/Entity.cs
@@ -3,12 +3,12 @@ namespace AuthService.Domain.Entities;
 public abstract class Entity
 {
     public Guid Id { get; protected set; }
-
-    public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
+    public DateTime CreatedAt { get; protected set; }
     public DateTime? UpdatedAt { get; protected set; }
 
     protected Entity()
     {
+        Id = Guid.NewGuid();
         CreatedAt = DateTime.UtcNow;
         UpdatedAt = DateTime.UtcNow;
     }
